c9bc71a8e2e61125578e882a780c1cf8ec38e5dc
laoxin.lx <laoxin.lx@alibaba-inc.com>
2017-03-13 14:13:58 +0800

    url访问 && document解析...

---
+++ src/main/java/crawler/request/DocumentGetter.java
@@ -0,0 +1,12 @@
+package crawler.request;
+
+import org.jsoup.nodes.Document;
+
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+public interface DocumentGetter {
+
+    Document getDocument(String url);
+
+}
---
+++ src/main/java/crawler/request/impl/DocumentGetterImpl.java
@@ -0,0 +1,38 @@
+package crawler.request.impl;
+
+import crawler.request.DocumentGetter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+/**
+ * Created by Administrator on 2017/3/13.
+ */
+public class DocumentGetterImpl implements DocumentGetter {
+
+    /**
+     * 访问url，并将其解析为Document类
+     *
+     * @param url
+     * @return
+     */
+    public Document getDocument(String url) {
+
+        Document doc = null;
+
+        try{
+            doc = Jsoup.connect(url)
+                    .ignoreContentType(true)
+                    .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31")
+                    .timeout(20000)
+                    //.cookies(cookies)
+                    .get();
+
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }finally {
+            return doc;
+        }
+    }
+}
