e9e9c99e80326c1c26667fe373fa18180d730390
laoxin.lx <laoxin.lx@alibaba-inc.com>
2017-03-14 22:08:38 +0800

    已完成分词...

---
+++ src/test/java/working/CrawleRun.java
@@ -0,0 +1,95 @@
+package working;
+
+import com.raul.bupt.dataobject.Article;
+import com.raul.bupt.parse.HtmlParser;
+import com.raul.bupt.request.DocumentGetter;
+import org.jsoup.nodes.Document;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数据爬取主程序
+ *
+ * Created by Administrator on 2017/3/13.
+ */
+public class CrawleRun {
+
+
+
+
+    public static void main(String[] args) throws IOException{
+
+        /**
+         * 获取bean对象
+         */
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        HtmlParser htmlParser = (HtmlParser) applicationContext.getBean("htmlparser");
+        DocumentGetter documentGetter = (DocumentGetter) applicationContext.getBean("documentgetter");
+
+        String url = "";
+        String nextUrl = "http://chuansong.me/account/ecigbar";
+        List<Article> articleList = new ArrayList<Article>();
+        int pageNum = 1;
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("article/articles.txt"));
+
+        while((!url.equals(nextUrl)) && nextUrl!=null) {
+
+            url = nextUrl;
+
+            documentGetter.setProxy();
+
+            Document doc = null;
+            try {
+                doc = documentGetter.getDocument(url);
+            }catch (Exception e) {
+                url = "";
+                continue;
+            }
+
+            List<Article> pageArticleList = htmlParser.getArticleList(doc);
+            for(int i=0;i<pageArticleList.size();i++) {
+
+                Article article = pageArticleList.get(i);
+                String articleLink = article.getLink();
+
+                Document articleDoc = null;
+                try{
+                    articleDoc = documentGetter.getDocument(articleLink);
+                }catch (Exception e) {
+
+                    System.out.println(e.getMessage() + "---" + articleLink);
+
+                    //404 error
+                    if(!e.getMessage().contains("404 error")) {
+                        i = i - 1;
+                    }else {
+                        e.printStackTrace();
+                    }
+                    continue;
+                }
+
+                String content = htmlParser.getArticleContent(articleDoc).replaceAll("\t","");
+                article.setContent(content);
+
+                System.out.println(article);
+                bw.write(article.toString() + "\r\n");
+                articleList.add(article);
+            }
+
+            System.out.println("当前爬取第" + pageNum + "页：已完成页面 " + url + " 的爬取； 当前article的数量为：" + articleList.size() + "...");
+
+            nextUrl = htmlParser.getNextPageUrl(doc);
+            pageNum += 1;
+        }
+
+        bw.flush();  bw.close();
+    }
+}
---
+++ src/test/java/working/SegmentRun.java
@@ -0,0 +1,50 @@
+package working;
+
+import com.raul.bupt.segment.WordParticiple;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.*;
+
+/**
+ * 分词主程序
+ *
+ * Created by Administrator on 2017/3/14.
+ */
+public class SegmentRun {
+
+
+    private static final String articleSavePath = "article/afterProcess.txt";
+
+    private static final int contentIndex = 2;
+
+    public static void main(String[] args) throws IOException{
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        WordParticiple wordParticiple = (WordParticiple) applicationContext.getBean("wordparticiple");
+
+        InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(new File(articleSavePath)));
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String temp = bufferedReader.readLine();
+
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("article/articlesTraining.txt"));
+        while(temp != null) {
+            String[] articleArray = temp.split("\t");
+            String content = articleArray[contentIndex];
+
+//            wordParticiple.findNewWords(content);
+//            wordParticiple.findKeyWords(content);
+
+            String segmentSentence = wordParticiple.wordSegmentWithoutStopWord(content);
+            System.out.println(segmentSentence);
+            bw.write(segmentSentence + "\r\n");
+
+            temp = bufferedReader.readLine();
+        }
+
+        bufferedReader.close();
+        bw.flush();  bw.close();
+    }
+}
